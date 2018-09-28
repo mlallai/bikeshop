11298bc60478e13bf0b5e309276e15340ccd9603
mlallai <matthieu.lallai@gmail.com>
2018-09-28 10:22:55 +0200

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var session = require("express-session");
 
-
+//var dataCardBike =[];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("before", req.session.dataCardBike);
+  if(req.session.dataCardBike  == undefined) {
+    req.session.dataCardBike = [];
+  }
   var dataBike = [
     {name: "Model BIKO45", url:"/images/bike-1.jpg", price: 679},
     {name: "Model ZOOK7", url:"/images/bike-2.jpg", price: 799},
@@ -13,8 +16,7 @@ router.get('/', function(req, res, next) {
     {name: "Model GEWO", url:"/images/bike-4.jpg", price: 1206},
     {name: "Model TITAN5", url:"/images/bike-5.jpg", price: 989},
     {name: "Model AMIG39", url:"/images/bike-6.jpg", price: 599}
-  ];
-  dataCardBike = [];
+  ]
   res.render('index', { dataBike:dataBike });
 });
 
@@ -25,33 +27,28 @@ router.get('/shop', function(req, res, next) {
 
 //ajout dans le panier
 router.post('/add-shop', function(req, res, next) {
-  if (req.session.dataCardBike == undefined) {
-    req.session.dataCardBike = [req.body];
-    res.render('shop', {dataCardBike: req.session.dataCardBike});
+  console.log(req.body);
+
+  var isUpdate = false;
+  for(var i=0; i <req.session.dataCardBike.length; i++) {
+    if (req.session.dataCardBike[i].name == req.body.name){
+      req.session.dataCardBike[i].quantity++;
+      isUpdate = true;
+    }
   }
-  else {
-    var index = req.body.position;
-    if(req.session.dataCardBike.some(function(el){
-      return el.name === req.body.name
-    })) {
-    res.render('shop', {dataCardBike: req.session.dataCardBike});}
-    else {
-    req.session.dataCardBike.push(req.body);
-    res.render('shop', {dataCardBike: req.session.dataCardBike});
-  };
-};
+
+  if(isUpdate == false){
+    req.session.dataCardBike.push(req.body)
+  }
+
+  res.render('shop', {dataCardBike: req.session.dataCardBike});
 });
-
-
-//   if (dataCardBike === )
-//   req.session.dataCardBike = req.body;
-//   dataCardBike.push(req.session.dataCardBike);
-//   res.render('shop', {dataCardBike});
-// });
 
 //suppr d'un element du panier
 router.get('/delete-shop', function(req, res, next) {
+
   req.session.dataCardBike.splice(req.query.position, 1)
+
   res.render('shop', {dataCardBike: req.session.dataCardBike});
 });
 
